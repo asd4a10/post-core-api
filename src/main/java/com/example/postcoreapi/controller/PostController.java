9ec8dc1f5d2b3dc877f09c7bfa7544511a55eac2
asd4a10 <asd4a10>
2022-03-27 19:54:41 +0600

    adding gateway and discovery server features

--- src/main/java/com/example/postcoreapi/controller/PostController.java
+++ src/main/java/com/example/postcoreapi/controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.postcoreapi.controller;
 import com.example.postcoreapi.model.PostModel;
 import com.example.postcoreapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,12 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/check")
     public String check() {
-        return "post-core-api is working";
+        return "post-core-api is working"+env.getProperty("local.server.port");
     }
 
     @PostMapping
--- src/main/java/com/example/postcoreapi/service/PostService.java
+++ src/main/java/com/example/postcoreapi/service/PostService.java
@@ -5,11 +5,7 @@ import com.example.postcoreapi.model.PostModel;
 import java.util.List;
 
 public interface PostService {
-//    Метод createPost() для создания заказа посылки
-//    Метод getAllPosts() для вывода списка посылок
-//    Метод getPostById() для вывода данных о посылке по ID
-//    Метод updatePostById() для обновления данных о посылке по ID
-//    Метод deletePostById() для удаления данных о посылке по ID
+
      void createPost(PostModel postModel);
 
      List<PostModel> getAllPosts();
